f8c02440a5266c0c563aeb8149f4ca55c2a7bdf5
Luke Ryan <lukeryan0@hotmail.co.uk>
2020-03-30 16:10:19 +0100

    Initial Commit - script with simple dollar cost average investment strategy

---
+++ README.md
@@ -0,0 +1,5 @@
+## Strategy Tester
+
+Currently, this houses a script which reads in monthly FTSE100 data and plots a graph of:
+ - The resulting capital at the end of an X year period of investing %1000 per month for every possible 30 year time slice in the data.
+ - The distribution of return on invested capital across all theoretical investors.
---
+++ script.py
@@ -0,0 +1,55 @@
+// Read the CSV
+// Sort it by the date
+// Imagine a set of investors, who each begin investing 1k into the FTSE 100 on a monthly basis and continue for 20 years
+// We want to calculate the capital they have at the end (through investing alone)
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+def readData(filepath):
+  raw = list(csv.reader(open(filepath), delimiter=','))[1:]
+  data = sorted(list(map(lambda x: (datetime.strptime(x[0], '%b %d, %Y'), x[4]), raw)), key = lambda x: x[0])
+  return data
+
+def capitalAppreciationModel(data, numMonths, monthlyAmount):
+  result = {}
+  
+  for i in xrange(0, len(data) - numMonths):
+    result[i] = [[0, 0.0]]
+    for j in xrange(i, i + numMonths):
+      quantity = monthlyAmount / float(data[j][1])
+      previousResult = result[i][-1]
+      newResult = [previousResult[0] + quantity, (previousResult[0] + quantity) * float(data[j][1])]
+      result[i].append(newResult)
+  
+  toPlot = map(lambda x: [x[0], x[1][-1]], result.items())
+  
+  x = []
+  y = []
+  
+  for item in toPlot:
+    x.append(item[0])
+    y.append(item[1][1])
+  
+  # Plot the return for each investor
+  plt.plot(x,y)
+  plt.plot(x, np.zeros([len(x),1])+(numMonths*monthlyAmount))
+  plt.show()
+  
+  # Plot the ROIC 
+  roic = ((np.asarray(y) / float(numMonths*monthlyAmount)) - 1)*100
+  plt.hist(roic, bins=xrange(-100,100,2))
+  plt.show()
+
+path = "C:\Users\Luke\Documents\Finance\FTSE_monthly.csv"
+workingData = readData(path)
+
+monthlyInvestment = 1000.0
+years = 20
+yearsInMonths = years * 12
+
+capitalAppreciationModel(workingData, yearsInMonths, monthlyInvestment)
+
+// We should ignore any yield initially
+// We could extend this to consider their drawdown after this initial 20 year period is over
